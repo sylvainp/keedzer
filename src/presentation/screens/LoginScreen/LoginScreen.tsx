b8be889ae93261a4c15f20a89d55b77cd6f726fa
Sylvain <sylvain@MBP-de-Sylvain-001.lan>
2023-07-19 16:26:28 +0200

    improve login ui

--- src/presentation/screens/LoginScreen/LoginScreen.tsx
+++ src/presentation/screens/LoginScreen/LoginScreen.tsx
@@ -8,19 +8,25 @@ import {useAppDispatch} from '../../../store/hooks';
 import theme from '../../theme';
 
 const styles = StyleSheet.create({
-  root: {backgroundColor: theme.color.backgroundColor},
-  titleContainer: {height: '50%'},
+  root: {backgroundColor: theme.color.backgroundColor, flex: 1},
+
+  imageContainer: {flex: 2},
   image: {alignSelf: 'center'},
+  titleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   title: {
     color: 'white',
-    fontSize: 44,
+    fontSize: 74,
     textAlign: 'center',
     shadowColor: 'white',
     shadowOffset: {width: 0, height: -1},
     shadowOpacity: 0.5,
   },
-  container: {
-    height: '50%',
+  buttonContainer: {
+    flex: 2,
+
     justifyContent: 'center',
   },
 
@@ -37,11 +43,13 @@ const LoginScreen = () => {
   return (
     <SafeAreaView style={styles.root}>
       <StatusBar barStyle={'light-content'} />
-      <View style={styles.titleContainer}>
+      <View style={styles.imageContainer}>
         <MusicSVG style={styles.image} />
+      </View>
+      <View style={styles.titleContainer}>
         <Text style={styles.title}>Keedzer</Text>
       </View>
-      <View style={styles.container}>
+      <View style={styles.buttonContainer}>
         <KDLoginDeezerButton
           onPress={() => {
             setIsLogging(true);
@@ -52,6 +60,7 @@ const LoginScreen = () => {
       <KDLoginDeezerView
         isVisible={isLogging}
         onDissmiss={(accessToken?: string) => {
+          console.log('onDissmiss');
           setIsLogging(false);
           if (accessToken) {
             saveAccessToken(accessToken);
